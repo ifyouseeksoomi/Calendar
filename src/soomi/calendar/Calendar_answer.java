b962a4dcbf8158aff4cf05a40c97963ec50727db
soomialexhwang <ifyouseeksoomi@gmail.com>
2020-06-02 20:41:10 +0900

    Update Calendar_answer - array

--- src/soomi/calendar/Calendar_answer.java
+++ src/soomi/calendar/Calendar_answer.java
@@ -2,7 +2,7 @@ package soomi.calendar;
 
 import java.util.Scanner;
 
-public class Calendar {
+public class Calendar_answer {
 	public static void main(String[] args) {
 		System.out.println("Hello, Calendar");
 		System.out.println();
@@ -24,32 +24,17 @@ public class Calendar {
 	
 		// 숫자(월)를 입력받아 그 달이 몇 개의 일로 구성되어 있는지, 즉
 		// 해당 달의 최대 일수 출력하기
+		// 배열 이용하여 해보
 		
-
+		System.out.println();
 		Scanner sc = new Scanner(System.in);
 		System.out.print("숫자를 입력하세요. : ");
 		int input = sc.nextInt();
 
-		switch (input) {
-
-		case 1:
-		case 3:
-		case 5:
-		case 7:
-		case 8:
-		case 10:
-		case 12:
-			System.out.println(input + "월은 31일로 구성되어 있습니다."); break;
-		case 4:
-		case 6:
-		case 9:
-		case 11:
-			System.out.println(input + "월은 30일로 구성되어 있습니다."); break;
-		case 2:
-			System.out.println(input + "월은 28일로 구성되어 있습니다."); break;
-		default: 
-			System.out.println("1부터 12사이의 숫자만 입력바랍니다."); 
-			
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		System.out.printf("%d은 %d일까지 있습니다.\n", input, maxDays[input-1]);
+		sc.close();
 			
 
 		}
@@ -57,4 +42,3 @@ public class Calendar {
 
 	}
 
-}
