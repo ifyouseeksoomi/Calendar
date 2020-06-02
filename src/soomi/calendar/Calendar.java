3ac3d9bd27138c863dddaafb692391e52af8fd41
soomialexhwang <ifyouseeksoomi@gmail.com>
2020-06-02 16:41:07 +0900

    Update calendar - my answer

--- src/soomi/calendar/Calendar.java
+++ src/soomi/calendar/Calendar.java
@@ -3,6 +3,19 @@ package soomi.calendar;
 public class Calendar {
 	public static void main(String[] args) {
 		System.out.println("Hello, Calendar");
-	}
+		System.out.println();
 
+		System.out.println("일 월 화 수 목 금 토 ");
+		System.out.println("-----------------");
+
+		for (int i = 1; i <= 28; i++) {
+			System.out.printf("%3d", i);
+
+			if (i % 7 == 0) {
+				System.out.println();
+
+			}
+
+		}
+	}
 }
